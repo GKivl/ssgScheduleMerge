37baf4edc5547a83e90281614a3eb6426231d4bb
Gustavs Kivlenieks <139682761+GKivl@users.noreply.github.com>
2024-11-01 07:05:35 +0200

    Implemented displaying schedule with css grid | not finished

--- src/index.js
+++ src/index.js
@@ -39,87 +39,89 @@ function regenContent() {
 	const classrooms = mainEduPageClient.getClassrooms()
 	const huePerTeachID = 360 / mainEduPageClient.getTeacherCount()
 
-	for(let day in schedule) {
-		let newCol = document.createElement("div")
-		newCol.className = "day"
+	// Probably, don't need this... Why is it here anyway?
+	// for(let day in schedule) {
+	// 	let newCol = document.createElement("div")
+	// 	newCol.className = "day"
+	//
+	// 	const date = new Date(day)
+	// 	let topRowElement = document.createElement("div")
+	// 	topRowElement.className = "topRow"
+	// 	topRowElement.innerHTML = `
+	// 		<h5>${new Intl.DateTimeFormat(locale, {weekday: 'long'}).format(date)}</h5>
+	// 		<p>${new Intl.DateTimeFormat(locale, {day: 'numeric', month: 'long'}).format(date)}</p>
+	// 	`
+	//
+	// 	newCol.appendChild(topRowElement)
+	//
+	// 	for(let period in periodTimes) {
+	// 		let newPeriod = document.createElement("div")
+	// 		newPeriod.className = "period"
+	//
+	// 		if(schedule[day][period] !== undefined) {
+	// 			const curPeriod = schedule[day][period]
+	// 			const curTeacher = teacherNames[curPeriod[0]["teacherIDs"][0]]
+	//
+	// 			newPeriod.innerHTML = `
+	// 				<div class="leftCol">
+	// 					<h6>${subjectNames[curPeriod[0]["subjectId"]]["short"]}</h6>
+	// 					<p class="taecher">${curTeacher["short"]}</p>
+	// 				</div>
+	// 				<span class="class">${classrooms[curPeriod[0]["classroomIDs"][0]] === undefined ? "" : classrooms[curPeriod[0]["classroomIDs"][0]]}</span>
+	// 			`
+	// 		}
+	//
+	// 		newCol.appendChild(newPeriod)
+	// 	}
+	//
+	// 	// scheduleEl.appendChild(newCol)
+	// }
 
-		const date = new Date(day)
-		let topRowElement = document.createElement("div")
-		topRowElement.className = "topRow"
-		topRowElement.innerHTML = `
-			<h5>${new Intl.DateTimeFormat(locale, {weekday: 'long'}).format(date)}</h5>
-			<p>${new Intl.DateTimeFormat(locale, {day: 'numeric', month: 'long'}).format(date)}</p>
-		`
-
-		newCol.appendChild(topRowElement)
-
-		for(let period in periodTimes) {
-			let newPeriod = document.createElement("div")
-			newPeriod.className = "period"
-
-			if(schedule[day][period] !== undefined) {
-				const curPeriod = schedule[day][period]
-				const curTeacher = teacherNames[curPeriod[0]["teacherIDs"][0]]
-
-				newPeriod.innerHTML = `
-					<div class="leftCol">
-						<h6>${subjectNames[curPeriod[0]["subjectId"]]["short"]}</h6>
-						<p class="taecher">${curTeacher["short"]}</p>
-					</div>
-					<span class="class">${classrooms[curPeriod[0]["classroomIDs"][0]] === undefined ? "" : classrooms[curPeriod[0]["classroomIDs"][0]]}</span>
-				`
-			}
-
-			newCol.appendChild(newPeriod)
-		}
-
-		// scheduleEl.appendChild(newCol)
-	}
-
-	// Top row stuff
-	let topRowEl = document.createElement("tr")
+	// Creating the elements for the top row
+	let n = 1
 	for(let day in schedule) {
 		let date = new Date(day)
-		let newTHEl = document.createElement("th")
-		newTHEl.className = "day"
+		let newTopRowEl = document.createElement("div")
+		newTopRowEl.className = "day"
 
-		newTHEl.innerHTML = `
+		newTopRowEl.style.gridColumn = `${n++}`
+		newTopRowEl.style.gridRow = '0'
+
+		newTopRowEl.innerHTML = `
 			<h5>${new Intl.DateTimeFormat(locale, {weekday: 'long'}).format(date)}</h5>
 			<p>${new Intl.DateTimeFormat(locale, {day: 'numeric', month: 'long'}).format(date)}</p>
 		`
 
-		topRowEl.appendChild(newTHEl)
+		scheduleEl.appendChild(newTopRowEl)
 	}
-	scheduleEl.appendChild(topRowEl)
 
 	// The rest
 	for(let period in periodTimes) {
-		let newTREl = document.createElement("tr")
-
+		let n = 1
 		for(let day in schedule) {
 			if(schedule[day][period] !== undefined) {
 				const curPeriod = schedule[day][period]
 				const curTeacher = teacherNames[curPeriod[0]["teacherIDs"][0]]
 
-				let newTDEl = document.createElement("td")
-				newTDEl.className = "period"
-				newTDEl.style.backgroundColor = `hsl(${Math.abs(curPeriod[0]["teacherIDs"][0]) * huePerTeachID}, 100%, 75%)`
-				newTDEl.innerHTML = `
-				<div>
-					<div class="leftCol">
-						<h6>${subjectNames[curPeriod[0]["subjectId"]]["short"]}</h6>
-						<p class="taecher">${curTeacher["short"]}</p>
-					</div>
-					<span class="class">${classrooms[curPeriod[0]["classroomIDs"][0]] === undefined ? "" : classrooms[curPeriod[0]["classroomIDs"][0]]}</span>
+				let newLessonEl = document.createElement("div")
+				newLessonEl.className = "lesson"
+
+				newLessonEl.style.gridRow = `${parseInt(period) +1} / span ${curPeriod[0].length}`
+				newLessonEl.style.gridColumn = `${n}`
+
+				newLessonEl.style.backgroundColor = `hsl(${Math.abs(curPeriod[0]["teacherIDs"][0]) * huePerTeachID}, 100%, 75%)`
+
+				newLessonEl.innerHTML = `
+				<div class="leftCol">
+					<h6>${subjectNames[curPeriod[0]["subjectId"]]["short"]}</h6>
+					<p class="taecher">${curTeacher["short"]}</p>
 				</div>
+				<span class="class">${classrooms[curPeriod[0]["classroomIDs"][0]] === undefined ? "" : classrooms[curPeriod[0]["classroomIDs"][0]]}</span>
 				`
 
-				if(curPeriod[0].length > 1)
-					newTDEl.rowSpan = curPeriod[0].length
-
-				newTREl.appendChild(newTDEl)
+				scheduleEl.appendChild(newLessonEl)
 			}
+			n++
 		}
-		scheduleEl.appendChild(newTREl)
 	}
 }
